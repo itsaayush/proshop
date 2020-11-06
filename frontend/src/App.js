1cee5bb8390eb1d41a7e7cec9c84d68ae4ca8b6d
Aayush <itsaayush128@gmail.com>
2020-11-06 07:58:19 +0530

    After Rating Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './Screens/HomeScreen'
+
 const App = () => {
   return (
     <>
-     <h1>Welcome to proshop</h1> 
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen></HomeScreen>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
